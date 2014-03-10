e8cf28e765b4e22bf0d03258b5ea1e66a960808f
GeHou <8008086@gmail.com>
2014-03-10 14:30:30 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
+
 define('app_dir', __DIR__ . '/..');
 define('engine_dir', app_dir . '/engine');
+
 require engine_dir . '/engine.php';
+require __DIR__.'/../vendor/autoload.php';
+
 engine::run();
